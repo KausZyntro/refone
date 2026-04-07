7854174b9f72d71f9c8c7ceac2dc98020ba9cccf
KausZyntro <kaustubhi@zyntro.in>
2026-04-07 17:34:57 +0530

    responsive bar in hero slider

--- src/components/home/ActionCards.jsx
+++ src/components/home/ActionCards.jsx
@@ -3,19 +3,17 @@ import "@/styles/ActionCards.css";
 
 const ActionCards = ({ items }) => {
   return (
-    <a href="/allProduct">
     <div className="action-container">
       {items.map((item, index) => (
-        <div key={index} className="action-card">
+        <a key={index} href="/allProduct" className="action-card">
           <div className="icon">{item.icon}</div>
           <div className="content">
             <h3>{item.title}</h3>
             <p>{item.subtitle}</p>
           </div>
-        </div>
+        </a>
       ))}
     </div>
-    </a>
   );
 };
 
