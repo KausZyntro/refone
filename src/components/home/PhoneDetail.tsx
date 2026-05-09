dcb0309fa95ad058c92734a12efb5ee1cbfadbd3
KausZyntro <kaustubhi@zyntro.in>
2026-05-09 15:46:20 +0530

    change in file name

--- src/components/home/PhoneDetail.tsx
+++ src/components/home/PhoneDetail.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import "@/styles/PhoneDetail.css";
 
 const categories = [
-  { name: "iPhone 14",id:"8", img: "/images/iphone14.jpg" },
-  { name: "iPhone 14 Pro",id:"6", img: "/images/iphone14Pro.jpg" },
+  { name: "iPhone 14",id:"8", img: "/images/iPhone14.jpg" },
+  { name: "iPhone 14 Pro",id:"6", img: "/images/iPhone14Pro.jpg" },
   { name: "iPhone 13", id:"9", img: "/images/iphone131.jpg" },
   { name: "iPhone 15", id:"4", img: "/images/iPhone15Pink.webp" },
-  { name: "iPhone 15 Pro", id:"2", img: "/images/iPhone15pro.webp" },
-  { name: "iPhone 15 Pro Max", id:"1", img: "/images/iPhone15promax.webp" },
-  { name: "iPhone 15 Plus", id:"3", img: "/images/iPhone15plus.jpeg" },
+  { name: "iPhone 15 Pro", id:"2", img: "/images/iphone15pro.webp" },
+  { name: "iPhone 15 Pro Max", id:"1", img: "/images/iphone15promax.webp" },
+  { name: "iPhone 15 Plus", id:"3", img: "/images/iphone15plus.jpeg" },
   { name: "iPhone 14 Pro Max", id:"5", img: "/images/iphone14promax.webp" },
   { name: "All Phones", isAll: true },
 ];
